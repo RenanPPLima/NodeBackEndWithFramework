8fb533e299c672b6bfdd4703ad03450b52dce7a7
Renan Lima <144637663+RenanPPLima@users.noreply.github.com>
2024-04-22 22:14:49 -0300

    Add files via upload

---
+++ controllers/serviceController.js
@@ -0,0 +1,34 @@
+const { Service: ServiceModel } = require("../models/Service");
+
+const serviceController = {
+    create: async(req, res) => {
+        try {
+            const service = {
+                name: req.body.name,
+                description: req.body.description,
+                price: req.body.price,
+                image: req.body.image,
+            };
+            
+        const response = await ServiceModel.create(service);
+        
+        res.status(201).json({ response, msg: "Service created successfully" });
+
+        } catch (error) {
+            console.log(error);
+            
+        }
+    },
+    getAll: async (req, res) => {
+        try {
+            const services = await ServiceModel.find();
+            
+            res.json(services);
+            
+        } catch (error) {
+            console.log(error);
+        }
+    }
+}
+
+module.exports = serviceController;
