f4bb0990e8d3ad6e2e099deaba1f6d13fd027c91
Renan Lima <144637663+RenanPPLima@users.noreply.github.com>
2024-04-24 20:56:20 -0300

    Update serviceController.js

--- controllers/serviceController.js
+++ controllers/serviceController.js
@@ -1,6 +1,7 @@
 const { Service: ServiceModel } = require("../models/Service");
 
 const serviceController = {
+
     create: async(req, res) => {
         try {
             const service = {
@@ -19,16 +20,58 @@ const serviceController = {
             
         }
     },
+
     getAll: async (req, res) => {
         try {
             const services = await ServiceModel.find();
             
             res.json(services);
+
             
         } catch (error) {
             console.log(error);
         }
+    },
+
+    get: async (req, res) => {
+        try {
+            const id = req.params.id;
+            const service = await ServiceModel.findById(id);
+
+            if(!service){
+                res.status(404).json({ msg: "Service not found." });
+                return;
+            }
+
+        res.json(service);
+
+        } catch (error) {
+            console.log(error);
+            console.log("Incorrect ID or ID pattern.")
+            
+        }
+    },
+
+    delete: async (req, res) => {
+        try {
+            const id = req.params.id;
+
+            const service = await ServiceModel.findById(id);
+
+            if(!service){
+                res.status(404).json({ msg: "Service not found." });
+                return;
+            }
+
+            const deletedService = await ServiceModel.findByIdAndDelete(id);
+
+            res.status(200).json({ deletedService, msg: "Service deleted successfully." })
+            
+        } catch (error) {
+            console.log(error);
+            console.log("Check ID or ID pattern.");
+        }
     }
-}
+};
 
 module.exports = serviceController;
